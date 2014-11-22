8627aedf8c89d1185f211129238f271872176863
sirfi <sir.fatih.isik@gmail.com>
2014-11-22 13:24:08 +0200

    Created

---
+++ bootstrap-grid-halfs-css-creator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace bootstrap_grid_halfs_css_creator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.AppendAllText("bootstrap-grid-halfs.css",GetBootstrapHalfs());
+            Console.WriteLine("Its Created");
+        }
+        public static string GetBootstrapHalfs(int gridColumnCount = 12, int gridGutterWidth = 30,int minScreen = 480)
+        {
+            var css = "";
+            var screens = new Dictionary<string, int> { { "xs", 480 }, { "sm", 768 }, { "md", 992 }, { "lg", 1200 } };
+            var types = new[] { "", "pull", "push", "offset" };
+            Func<string, string, string, string> className = (screen, col, type) => ".col-" + screen + (string.IsNullOrEmpty(type) ? "" : "-" + type) + "-" + col + "-half";
+            Func<string, string, string> classNames = (screen, type) => Enumerable.Range(0, gridColumnCount).Select(x => x.ToString()).Aggregate("", (current, i) => current + (string.IsNullOrEmpty(current) ? "" : ",") + (className(screen, i, type)));
+            Func<string, string> classNamesForAllScreen = (type) => screens.Keys.Aggregate("", (current, screenName) => current + (string.IsNullOrEmpty(current) ? "" : ",") + (classNames(screenName, type)));
+            Func<string, string> classNamesForAllTypes = (screen) => types.Aggregate("", (current, type) => current + (string.IsNullOrEmpty(current) ? "" : ",") + (classNames(screen, type)));
+            Func<string> classNamesForAllScreenAndAllTypes = () => types.Aggregate("", (current, type) => current + (string.IsNullOrEmpty(current) ? "" : ",") + (classNamesForAllScreen(type)));
+            css += classNamesForAllScreen("") + "{" + "position: relative;min-height: 1px;padding-right: " + gridGutterWidth / 2 + "px;padding-left: " + gridGutterWidth / 2 + "px;" + "}";
+            foreach (var screen in screens)
+            {
+                if (screen.Value > minScreen)
+                    css += "@media (min-width: " + screen.Value + "px) {";
+
+                css += classNames(screen.Key, "") + "{float: left;}";
+
+                for (int i = 0; i < gridColumnCount; i++)
+                {
+                    css += className(screen.Key, i.ToString(), "") + "{width: " + (((i + 0.5) / gridColumnCount) * 100).ToString().Replace(',', '.') + "%;}";
+                    css += className(screen.Key, i.ToString(), "pull") + "{right: " + (((i + 0.5) / gridColumnCount) * 100).ToString().Replace(',', '.') + "%;}";
+                    css += className(screen.Key, i.ToString(), "push") + "{right: " + (((i + 0.5) / gridColumnCount) * 100).ToString().Replace(',', '.') + "%;}";
+                    css += className(screen.Key, i.ToString(), "offset") + "{margin-left: " + (((i + 0.5) / gridColumnCount) * 100).ToString().Replace(',', '.') + "%;}";
+                }
+
+                if (screen.Value > minScreen)
+                    css += "}";
+            }
+            return css;
+        }
+    }
+}
---
+++ bootstrap-grid-halfs-css-creator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("bootstrap-grid-halfs-css-creator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("bootstrap-grid-halfs-css-creator")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7e97e584-255f-47ba-a17b-eb8828c7f681")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
